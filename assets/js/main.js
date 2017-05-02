9a6a9ad045d14a53b57a110e89cd51354031f597
Simone Picciani <simone.picciani@spindox.it>
2017-05-02 17:50:38 +0200

    porting of new graphic

---
+++ assets/js/main.js
@@ -0,0 +1,82 @@
+/*
+	Miniport by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	skel
+		.breakpoints({
+			desktop: '(min-width: 737px)',
+			tablet: '(min-width: 737px) and (max-width: 1200px)',
+			mobile: '(max-width: 736px)'
+		})
+		.viewport({
+			breakpoints: {
+				tablet: {
+					width: 1080
+				}
+			}
+		});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on mobile.
+			skel.on('+mobile -mobile', function() {
+				$.prioritize(
+					'.important\\28 mobile\\29',
+					skel.breakpoint('mobile').active
+				);
+			});
+
+		// CSS polyfills (IE<9).
+			if (skel.vars.IEVersion < 9)
+				$(':last-child').addClass('last-child');
+
+		// Scrolly.
+			$window.load(function() {
+
+				var x = parseInt($('.wrapper').first().css('padding-top')) - 15;
+
+				$('#nav a, .scrolly').scrolly({
+					speed: 1000,
+					offset: x
+				});
+
+			});
+
+	});
+
+	/*
+		Custom JS code
+	*/
+
+	// Motto update
+    var motti = [ 
+        "La pyBissa de Milan",
+        "Il serpent[1] di Milano",
+        "Python Milano's Flying Cathedral",
+        "Il secondo serpente di Milano, dopo il biscione",
+        ];
+    var motto = motti[Math.floor(Math.random() * motti.length)];
+    $('#motto').text('"""' + motto + '"""');
+
+	// Open external link in new tab
+	$("a[href^='https://']").attr("target","_blank");
+
+})(jQuery);
+
